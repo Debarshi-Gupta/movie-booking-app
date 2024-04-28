dc87d3b7a301543811c6feed23543a015456b1b7
Fallen-Angel61 <debarshigupta47@gmail.com>
2024-04-28 10:30:34 +0530

    Setup Spring Boot Project

---
+++ src/main/java/com/projects/moviebookingapp/MovieBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.projects.moviebookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingAppApplication.class, args);
+	}
+
+}
